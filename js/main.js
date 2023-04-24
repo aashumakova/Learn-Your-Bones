838ef87aea97beeade572aefe6aefd81be620b18
aashumakova <aashumakova@gmail.com>
2023-04-24 14:03:47 -0700

    random word generated

--- js/main.js
+++ js/main.js
@@ -1,13 +1,48 @@
 /*----- constants -----*/
-const AUDIO = new Audio('https://assets.mixkit.co/sfx/preview/mixkit-simple-countdown-922.mp3');
 
+const words = {
+    arm: ['humerus', 'ulna', 'radius'],
+    leg: ['femur', 'tibia', 'fibula'],
+    skull: ['frontal', 'occipital', 'temporal'],
+
+}
 /*----- app's state (variables) -----*/
+let secretWord = '';
+let count = 0;
+let winCount = 0;
+
 
 
 /*----- cached element references -----*/
+const wordsContainer = document.getElementById('words-container');
+const letterContainer = document.getElementById('letter-container');
+const userInputSection = document.getElementById('user-input-section');
+const canvas = document.getElementById('canvas');
+const resultText = document.getElementById('result-text');
+const playAgainButton = document.getElementById('play-again-button');
 
 
 /*----- event listeners -----*/
+playAgainButton.addEventListener('click', init)
 
+// /*----- functions -----*/
+init()
 
-/*----- functions -----*/
+function init(){
+// computer generates a random word
+    const wordArray = words['arm'];
+    secretWord = wordArray[Math.floor(Math.random() * wordArray.length)];
+    console.log(secretWord)
+}
+    
+
+    //update the input section to show empty slots for each letter
+    // let displayItem = secretWord.split('').map(function(char) {
+    //     return '<span class="dashes"></span>';
+    //   }).join('');
+    //   userInputSection.innerHTML = displayItem;
+    //   console.log(displayItem)
+// let displayItem = secretWord.split('').map(function(char) {
+//     return '';
+//     }).join('');
+//     userInputSection.innerHTML = displayItem;
