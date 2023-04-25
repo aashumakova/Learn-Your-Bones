4b1b0695ab3e0ea8c7a56b69c2f941290286dff2
aashumakova <aashumakova@gmail.com>
2023-04-25 16:05:14 -0700

    working on recognizing letters from user input

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ let secretWord = '';
 let count = 0;
 let inputSection = '';
 let winCount = 0;
+let displayWord = [];
 
 
 
@@ -28,32 +29,98 @@ const letterButtonsEl = document.querySelector('#letter-container');
 /*----- event listeners -----*/
 playAgainButton.addEventListener('click', init)
 
-letterButtonsEl.addEventListener('click', function(event) {
-    const buttonValue = event.target.textContent;
-    event.target.disabled = 'true';
-    console.log('Button clicked:', buttonValue);
-  });
 wordCategoriesButton.addEventListener('click', function (event) {
     const categoryValue = event.target.textContent;
     event.target.disabled = 'true';
-    console.log('Category button clicked:', categoryValue);
+    const wordArray = words[categoryValue];
+    secretWord = wordArray[Math.floor(Math.random() * wordArray.length)];
+    console.log(secretWord)
+    // console.log('Category button clicked:', categoryValue);
+     //function createDisplayWord(){
+            // let displayWord = [];
+            for ( let i = 0; i < secretWord.length; i++) {
+                displayWord[i] = 'x';
+            }
+            console.log(displayWord)
+        
+            displayWord.forEach(function(letterSlot) {
+            let letterSlotSpot = document.createElement('p');
+            letterSlotSpot.innerHTML = letterSlot;
+            userInputSection.appendChild(letterSlotSpot);
+            }
+            )
+        //}
 })
+
+letterButtonsEl.addEventListener('click', function(event) {
+    const letter = event.target.textContent;
+    event.target.disabled = 'true';
+    console.log('Button clicked:', letter);
+    // const letter = buttonValue;
+    // displayWordSplit = displayWord.split('');
+    //function checkLetter () {
+        // while (count < 5) {
+        //     // update display word array with player's move if the letter word is correct
+        //     for ( var j = 0; j < secretWord.length; j++ ) {
+        //         // if the letter is in the word
+        //         if (secretWord[j] === letter) {
+        //             // update the displayWord
+        //             displayWord[j] = letter;
+        //             winCount +=1;
+        //             if (winCount === secretWord.length) {
+        //                 resultText.innerHTML = `Good job! We have a future healthcare professional here!!!`;
+        //             }
+        //         } else {
+        //             // add a strike
+        //             count += 1;
+        //         }
+        //     }
+        //     }
+   //}
+   if (secretWord.includes(letter)) {
+    for ( var j = 0; j < secretWord.length; j++ ) {
+        if (secretWord[j] === letter) {
+            // update the displayWord
+            displayWord[j] = letter;
+            winCount +=1;
+            if (winCount === secretWord.length) {
+                resultText.innerHTML = `Good job! We have a future healthcare professional here!!!`;
+            }
+        } else {
+            // add a strike
+            count += 1;
+        }
+    }
+   }
+
+
+  });
+// wordCategoriesButton.addEventListener('click', function (event) {
+//     const categoryValue = event.target.textContent;
+//     event.target.disabled = 'true';
+//     const wordArray = words[categoryValue];
+//     secretWord = wordArray[Math.floor(Math.random() * wordArray.length)];
+//     console.log(secretWord)
+//     // console.log('Category button clicked:', categoryValue);
+//      //function createDisplayWord(){
+//             // let displayWord = [];
+//             for ( let i = 0; i < secretWord.length; i++) {
+//                 displayWord[i] = 'x';
+//             }
+//             console.log(displayWord)
+        
+//             displayWord.forEach(function(letterSlot) {
+//             let letterSlotSpot = document.createElement('p');
+//             letterSlotSpot.innerHTML = letterSlot;
+//             userInputSection.appendChild(letterSlotSpot);
+//             }
+//             )
+//         //}
+// })
 
 
 // /*----- functions -----*/
 // need a function to pull each item word category aka a key from the object onto the game board   
-// function displayWordOptions () {
-//     wordsContainer.innerHTML +=`<h1>Select your body parts</h1>`;
-//     let wordCategories = Object.keys(words);
-//     console.log(wordCategories)
-//     wordCategories.forEach(function(wordCategory){
-//     let wordCategoriesButton = document.createElement('div');
-//     wordCategoriesButton.innerHTML = wordCategory;
-//     wordsContainer.appendChild(wordCategoriesButton);
-//     })
-// }
-// displayWordOptions()
-
 function displayWordOptions () {
         wordsContainer.innerHTML +=`<h1>Select your body parts</h1>`;
         let wordCategories = Object.keys(words);
@@ -69,50 +136,26 @@ function displayWordOptions () {
 init()
 
 function init() {
-// computer generates a random word
-    const wordArray = words.arm;
-    // const wordArray = categoryValue;
-    secretWord = wordArray[Math.floor(Math.random() * wordArray.length)];
-    console.log(secretWord)
-//update the input section to show empty slots for each letter
-    // function createDisplayWord(){}
-    let displayWord = [];
-    for ( let i = 0; i < secretWord.length; i++) {
-        displayWord[i] = 'x';
-    }
-    console.log(displayWord)
-
-    displayWord.forEach(function(letterSlot) {
-    let letterSlotSpot = document.createElement('p');
-    letterSlotSpot.innerHTML = letterSlot;
-    userInputSection.appendChild(letterSlotSpot);
-    }
-    )
 }
-//update the input section to show empty slots for each letter
-// let displayWord = secretWord.split('').map(function(char) {
-//     return '';
-//     }).join('');
-//     userInputSection.innerHTML = displayItem;
 
 // player only has 5 tries 
 //if letter clicked is present in the word (e.target) from the clicked letter
-function checkLetter () {
-    while (count < 5) {
-        // update display word array with player's move if the letter word is correct
-        for ( var j = 0; j < secretWord.length; j++ ) {
-            // if the letter is in the word
-            if (secretWord[j] === letter) {
-                // update the displayWord
-                displayWord[j] = letter;
-                winCount +=1;
-                if (winCount === secretWord.length) {
-                    resultText.innerHTML = `Good job! We have a future healthcare professional here!!!`;
-                }
-            } else {
-                // add a strike
-                count += 1;
-            }
-        }
-        }
-}
+// function checkLetter () {
+//     while (count < 5) {
+//         // update display word array with player's move if the letter word is correct
+//         for ( var j = 0; j < secretWord.length; j++ ) {
+//             // if the letter is in the word
+//             if (secretWord[j] === letter) {
+//                 // update the displayWord
+//                 displayWord[j] = letter;
+//                 winCount +=1;
+//                 if (winCount === secretWord.length) {
+//                     resultText.innerHTML = `Good job! We have a future healthcare professional here!!!`;
+//                 }
+//             } else {
+//                 // add a strike
+//                 count += 1;
+//             }
+//         }
+//         }
+// }
